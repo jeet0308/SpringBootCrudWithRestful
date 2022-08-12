89859fb45d6ec5c0ebc87f0ad937cebbc6acfcbe
jeet0308 <jitsingh0308@gmail.com>
2022-08-12 17:12:59 +0530

    SBCWR-1:Create Project And Create Required package

---
+++ src/main/java/com/BikkadIT/SpringBootUsingRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsingRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsingRestfulApplication.class, args);
+	}
+
+}
